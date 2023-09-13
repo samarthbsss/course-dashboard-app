06b3681e4ce2230ab0d7694b49c77bea49ea225f
Samarth <samarthbsacharya@gmail.com>
2023-09-13 17:40:18 +0530

    first commit

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-white text-white shadow-lg">
+     
+    
+      </div>
+    
+  );
+};
+
+export default Sidebar;
